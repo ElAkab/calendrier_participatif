c52182ab16f6c9ea3635b288dabfe6cfc8486897
Adam <elkabir2005@gmail.com>
2025-06-02 18:23:04 +0200

    Corrige chemin pour servir index.html depuis backend (encore 2)

--- backend/server.js
+++ backend/server.js
@@ -114,7 +114,7 @@ app.delete("/delete-user/:userName", (req, res) => {
 app.use(express.static(path.join(__dirname, "..")));
 
 app.get("/", (req, res) => {
-	res.sendFile(path.join(__dirname, "..", "index.html"));
+	res.sendFile(path.join(__dirname, "..", "resultats.html"));
 });
 
 app.listen(PORT, () => {
