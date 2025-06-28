c25786dfcbdd42354c480f3a298ef6217f04ca16
Adam <elkabir2005@gmail.com>
2025-06-28 17:13:08 +0200

    Port : 4000 to 3000.

--- no-index/frontend/script.js
+++ no-index/frontend/script.js
@@ -3,7 +3,7 @@ console.log("JS chargé");
 const BASE_URL =
 	window.location.hostname === "localhost" ||
 	window.location.hostname === "127.0.0.1"
-		? "http://localhost:4000"
+		? "http://localhost:3000"
 		: "https://calendrier-participatif-public.onrender.com";
 
 function safeParseJSON(raw) {
